452a43b533f74f3b47b91873f6aede205ba4a25a
GilleanKosu <antoniogarciaillescas@gmail.com>
2019-02-24 19:46:30 +0100

    prueba

--- exChange/src/Controller/RegistrationController.php
+++ exChange/src/Controller/RegistrationController.php
@@ -25,8 +25,6 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        
-
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
